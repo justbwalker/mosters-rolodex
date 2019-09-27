5f423cc105f4ee345fe51833afc29448a0eddb75
Brian German Walker <justbwalker@hotmail.com>
2019-09-27 00:17:01 -0300

    Updates

---
+++ precache-manifest.7510ce81974c9815af1775829d5bcdcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991251d0238bcf3b0715b956dac9bd30",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2d41de1f36b2c873001",
+    "url": "/monsters-rolodex/static/css/main.c0c91524.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "e2d41de1f36b2c873001",
+    "url": "/monsters-rolodex/static/js/main.2525cbdb.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
